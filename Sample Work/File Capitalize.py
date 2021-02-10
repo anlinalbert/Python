93b664687ebb6b8f1d9b6e7ba992e1790771aaa3
anlinalbert <anlinalbert@gmail.com>
2021-02-10 21:58:41 +0530

    Completed sample program Q4 dated 10/02/2021.

---
+++ Sample Work/File Capitalize.py
@@ -0,0 +1,10 @@
+# Capitalize each word in a file
+
+def capitalize():
+    for i in f:
+        cap = i.title()
+        print(cap)
+
+
+f = open('D:\MCA\Sem 1\Python\Sample Work\input.txt', 'r')
+capitalize()
