617018c24e2f36991bf413355d49b32b1d7e3462
anlinalbert <anlinalbert@gmail.com>
2021-02-11 11:52:47 +0530

    Fixed a issue regarding specifying file path in lab program number 44.

--- LAB Work/44. Append Contents.py
+++ LAB Work/44. Append Contents.py
@@ -1,8 +1,14 @@
 # Append user input to file
 
+# '\\' means only '\' otherwise error
+path = 'D:\MCA\Sem 1\Python\LAB Work\\'
+
 filename = input("Enter filename: ")
 
-with open(filename, 'a') as f:
+# combines path and filename
+new = path + filename
+
+with open(new, 'a') as f:
     f.write(input())
 
 f.close()
