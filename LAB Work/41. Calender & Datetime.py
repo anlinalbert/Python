d3cae6078ece5ee5d07afd5dcd0672c63ba1e328
anlinalbert <anlinalbert@gmail.com>
2021-02-19 17:58:18 +0530

    One error corrected in lab program 41.

--- LAB Work/41. Calender & Datetime.py
+++ LAB Work/41. Calender & Datetime.py
@@ -3,7 +3,7 @@
 import calendar, datetime
 
 # to print feb 2021
-print(calendar.calendar(2021, 2))
+print(calendar.month(2021, 2))
 
 # current date & time
 print("Date & time:", datetime.datetime.now())
